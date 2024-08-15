f44869819f58adf4e89caec717557307564e51f0
abolfazl <abolfazlghotilo@gmail.com>
2024-08-15 15:54:11 +0330

    fit:add container and colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors:{
+      transparent:"transparent",
+      "bg": "#1E1E1E"
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '5rem',
+        '2xl': '1rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
