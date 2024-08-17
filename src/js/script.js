efa24cca98c09ae2ae8fa1195c3462b438596821
abolfazl <abolfazlghotilo@gmail.com>
2024-08-17 17:09:24 +0330

    fit: first section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
+let $ = document
+let humbermenu = $.getElementById("humbermenu")
+let navbar = $.getElementById("navbar")
+let close = $.getElementById("close")
 
-
-
+humbermenu.addEventListener("click" , function(){
+    navbar.classList.remove("hidden")
+})
+close.addEventListener("click" , function(){
+    navbar.classList.add("hidden");
+})
