0f2aad2c448bd2130c0c865359beb85e449ed558
abolfazl <abolfazlghotilo@gmail.com>
2024-09-03 14:49:05 +0330

    fit: add Go Top icon and close desktop view

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,23 @@ humbermenu.addEventListener("click" , function(){
 close.addEventListener("click" , function(){
     navbar.classList.add("hidden");
 })
+
+let mybutton = document.getElementById("gotop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
     AOS.init({
     duration: 800,
     });
